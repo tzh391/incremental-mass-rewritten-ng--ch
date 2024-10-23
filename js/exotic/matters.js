c80d7bb37f15e1b06d68368111eb75831a777b5b
qwqe198 <146399264+qwqe198@users.noreply.github.com>
2024-10-23 22:26:16 +0800

    Add files via upload

---
+++ js/exotic/matters.js
@@ -0,0 +1,108 @@
+const MATTERS = {
+    gain(i) {
+		let base=(i==0)?player.bh.dm : player.exotic.matters[i-1]
+		if(base.slog().lt(2))return E(0)
+		let x = Decimal.pow(10,base.slog().sub(1).pow(MATTERS.pow().mul(hasTree('ax16')?base.slog().div(3).max(1):1))).sub(10);
+		if(i==1 && hasTree('qp15'))x = x.mul(treeEff('qp15'));
+		if(i==2 && hasElement(513))x = x.mul(tmp.elements.effect[513]||1);
+		if(i<MATTERS_LENGTH-1 && hasElement(548))x = x.mul(player.exotic.matters[i+1].add(1).pow(MATTERS.extendPow()));
+		return x;
+    },
+    eff(i) {
+		let base=player.exotic.matters[i]
+		if(player.chal.active==23)base=E(0)
+		if(i==0||i==3||i==11){
+			let x = base.add(1).cbrt();
+			if(x.gte(1e10))x = x.log10().pow(10);
+			return x;
+		}
+		if(i==1){
+			let x = base.add(1).log10().softcap(1e9,2,0);
+			if(hasTree("qp34"))x = x.pow(1.5);
+			if(hasTree("qp41"))x = x.pow(1.5);
+			if(x.gte(100))x = x.log10().mul(50);
+			return E(0.9).pow(x);
+		}
+		if(i==2||i==10){
+			return base.add(1).log10().add(1).log10();
+		}
+		if(i==4){
+			return base.add(1).log10().add(1).log10().pow(2).mul(50000).softcap(2500000,2,0);
+		}
+		if(i==5||i==6||i==7||i==9){
+			return base.add(1).log10().add(1).log10().add(1).log10().add(1);
+		}
+		if(i==8){
+			return base.add(1).log10().add(1).log10().add(1).log10().add(1).log10().div(10);
+		}
+		if(i==12){
+			let x = base.add(1).log10().add(10).log10();
+			if(hasChargedElement(233))x = x.pow(x);
+			return x;
+		}
+    },
+    exeff() {
+		if(hasTree('ax9'))return player.exotic.points.add(1e10).log10().sqrt().div(2.63).log10().mul(10)
+		if(player.exotic.points.gte('1e360'))return player.exotic.points.add(10).log10().div(10).sqrt().mul(0.6).min(5).mul(hasElement(495)?2:1).min(10);
+		if(player.exotic.points.gte('1e260'))return player.exotic.points.add(10).log10().div(50).sub(3.6).min(5).mul(hasElement(495)?2:1).min(10);
+		return player.exotic.points.add(10).log10().div(100).sub(1).min(5).mul(hasElement(495)?2:1).min(10);
+    },
+    pow() {
+		let x = E(1)
+		if (hasTree('qp10')) x = x.add(0.3)
+		if (hasElement(487)) x = x.add(MATTERS.exeff());
+		if (hasElement(490)) x = x.add(0.4)
+		if (hasElement(492)) x = x.add(EXOTIC.dsEff().me)
+		if (hasElement(493)) x = x.add(0.3)
+		if (hasElement(508)) x = x.add(tmp.elements.effect[508]||0)
+		if (hasElement(512)) x = x.add(EXOTIC.abEff().me)
+		if (hasElement(524)) x = x.add(EXOTIC.drEff().me)
+		if (hasElement(544)) x = x.add(tmp.elements.effect[544]||0)
+		if (hasElement(545)) x = x.add(tmp.chal.eff[23]||0)
+		if (hasTree('qp17')) x = x.add(treeEff('qp17',0))
+		if (hasTree('ax5')) x = x.add(treeEff('ax5',0))
+		if (hasTree('ax22')) x = x.add(0.1)
+		if (hasTree('ax35')) x = x.add(0.1)
+		if (hasTree('ax38')) x = x.add(0.1)
+		if(player.superCluster.gte(13))x = x.add(SUPERNOVA_CLUSTER.effects.eff6());
+		if(player.exotic.dark_run.upgs[15].gte(1))x = x.add(tmp.dark_run.upgs[15].eff);
+		return x
+    },
+    extendPow() {
+		let x = E(0)
+		if (hasElement(548)) x = x.add(player.exotic.fss.div(100));
+		return x
+    },
+	fssBase(){
+		let x = E(0);
+		for(var i=0;i<MATTERS_LENGTH;i++){
+			x = x.add(player.exotic.matters[i].add(1).log10().add(1).log10());
+		}
+		return x;
+	},
+	fssReq(){
+		return E(40).mul(Decimal.pow(1.1,player.exotic.fss));
+	},
+	fssEff(){
+		let x = MATTERS.fssBase().pow(player.exotic.fss).sqrt();
+		if(hasTree('ax7'))x = x.pow(treeEff('ax7')||1);
+			return x;
+	},
+	fssReset(force=false){
+		if(!force)if(MATTERS.fssBase().lt(MATTERS.fssReq()))return;
+		if(!force)if((confirm("Are you sure to reset for a Final Star Shard? It will force an Exotic Reset, and reset your Exotic Matter!")?!confirm("ARE YOU SURE ABOUT IT???"):true)) return
+		if(!force)player.exotic.fss = player.exotic.fss.add(1);
+		player.exotic.matters = getPlayerData().exotic.matters;
+		player.exotic.points = E(0);
+		player.prestigeMass = E(0);
+		player.prestigeRP = E(0);
+		player.prestigeBH = E(0);
+		player.prestigeBHC = E(0);
+		player.prestigeDM = E(0);
+		player.prestigeAT = E(0);
+		player.prestigeTickspeed = E(0);
+		SUPERNOVA_CLUSTER.reset(true);
+	}
+}
+
+const MATTERS_LENGTH = 13;
---
+++ js/quantum/primordium.js
@@ -0,0 +1,187 @@
+const PRIM = {
+    unl() { return hasTree('unl2') },
+    getTheorems() {
+        let b = tmp.prim.t_base
+        let x = player.qu.bp.max(1).log(b).mul(hasChargedElement(200)?2.9282:hasChargedElement(166)?2.662:hasChargedElement(152)?2.42:hasChargedElement(131)?2.2:2)
+        return x.floor()
+    },
+    getNextTheorem() {
+        let b = tmp.prim.t_base
+        let x = E(b).pow(player.qu.prim.theorems.add(1).div(hasChargedElement(200)?2.9282:hasChargedElement(166)?2.662:hasChargedElement(152)?2.42:hasChargedElement(131)?2.2:2))
+
+        return x
+    },
+    spentTheorems() {
+        let x = E(0)
+        for (let i = 0; i < player.qu.prim.particles.length; i++) x = x.add(player.qu.prim.particles[i])
+        return x
+    },
+    particle: {
+        names: ["Delta [Δ]","Alpha [Α]","Omega [Ω]","Sigma [Σ]","Phi [Φ]","Epsilon [Ε]","Theta [Θ]","Beta [Β]"],
+        weight: [6,6,6,6,2,2,2,1],
+        total_w: 31,
+        chance: [],
+
+        eff: [
+            p=>{
+                let x = p.add(1).root(2)
+                return x
+            },
+            p=>{
+                let x = [p.root(3).div(5).add(1),p.pow(1.25).add(1)]
+                return x
+            },
+            p=>{
+                let x = [p.root(3).div(5).add(1),E(3).pow(p.pow(0.75))]
+                return x
+            },
+            p=>{
+                let x = [p.root(3).div(5).add(1),E(2).pow(p.pow(0.75))]
+                return x
+            },
+            p=>{
+                let x = p.add(1).root(10)
+                return x
+            },
+            p=>{
+                let x = [p.root(3).div(10).mul(hasChargedElement(95)?10:1), p.root(3).pow((QCs.active()||hasTree('prim3a'))?2:1).pow((!QCs.active() && hasElement(200))?1.75:1).pow((!QCs.active() && hasChargedElement(95))?1.2:1).mul(hasChargedElement(95)?10:1)]
+                return x
+            },
+            p=>{
+                let x = [E(5).pow(p.pow(0.75)), p.root(5).div(10).add(1)]
+                return x
+            },
+            p=>{
+                if (hasElement(107)) p = p.mul(2)
+                let x = p.pow(0.9).mul(2)
+                if (hasChargedElement(107)) x = p.pow(0.95).mul(100)
+				if (player.gc.active && !hasElement(555)){
+					let y = new Decimal(200000);
+					if(hasElement(362))y = y.add(150000);
+					while(x.gte(y)){
+						x = x.div(y.div(100000)).log10().mul(y.div(5));
+						y = y.add(100000);
+					}
+				}
+				if(x.gte(1e49) && !hasElement(529))x = Decimal.pow(10,x.log10().sqrt().mul(7));
+				if(hasElement(427)) x = x.mul(10)
+                return x
+            },
+        ],
+        effDesc: [
+            x=>{ return `Boost Stronger Power by ${format(x)}x` },
+            x=>{ return `Boost Rage Powers gain by ^${format(x[0])} /<br> Boost Non-Bonus Tickspeed by ${format(x[1])}x` },
+            x=>{ return `Boost Dark Matters gain by ^${format(x[0])} /<br> Boost BH Condenser Power by ${format(x[1])}x` },
+            x=>{ return `Boost Atoms gain by ^${format(x[0])} /<br> Boost Cosmic Ray Power by ${format(x[1])}x` },
+            x=>{ return `Boost Higgs Boson's effect by ${format(x)}x` },
+            x=>{ return `Add ${format(x[0])} to base from Fermions gain ` + (hasTree("prim3") ? ` /<br> Add ${format(x[1])} free tiers to Fermions` : "") },
+            x=>{ return `Boost all Radiations gains by ${format(x[0])}x` + (hasTree("prim2") ? ` /<br> Make all Radiations effects ${format(x[1])}x stronger` : "") },
+            x=>{ return `使超新星所有一阶折算延迟${format(x)}次出现` },
+        ],
+    },
+}
+
+function giveRandomPParticles(v, max=false) {
+    if (!PRIM.unl()) return
+	if (hasTree('prim8'))return;
+	if (hasTree('prim4') && PRIM.particle.weight[0]){
+		PRIM.particle.weight[0]=0;
+		PRIM.particle.total_w-=6;
+		calcPartChances();
+	}
+	if (hasTree('prim5') && PRIM.particle.weight[2]){
+		PRIM.particle.weight[2]=0;
+		PRIM.particle.total_w-=6;
+		calcPartChances();
+	}
+	if (hasTree('prim6') && PRIM.particle.weight[1]){
+		PRIM.particle.weight[1]=0;
+		PRIM.particle.total_w-=6;
+		calcPartChances();
+	}
+	if (hasTree('prim7') && PRIM.particle.weight[3]){
+		PRIM.particle.weight[3]=0;
+		PRIM.particle.total_w-=6;
+		calcPartChances();
+	}
+    let s = max?tmp.prim.unspent:E(v)
+    if (!max) s = s.min(tmp.prim.unspent)
+
+    let tw = PRIM.particle.total_w
+    let s_div = s.div(tw).floor()
+    let sm = s.mod(tw).floor().toNumber()
+
+    for (let x in PRIM.particle.names) player.qu.prim.particles[x] = player.qu.prim.particles[x].add(s_div.mul(PRIM.particle.weight[x]))
+    for (let x = 0; x < sm; x++) {
+        let c = Math.random()
+        for (let y in PRIM.particle.chance) if (c <= PRIM.particle.chance[y]) {
+            player.qu.prim.particles[y] = player.qu.prim.particles[y].add(1)
+            break
+        }
+    }
+
+    updatePrimordiumTemp()
+}
+
+function respecPParticles() {
+	if (hasTree('prim8'))return;
+    if (confirm("您确定要返还所有粒子吗？")) {
+        for (let i =0; i < 8; i++) player.qu.prim.particles[i] = E(0)
+        QUANTUM.doReset()
+    }
+}
+
+function calcPartChances() {
+    var sum = 0
+    for (let x in PRIM.particle.names) {
+        sum += PRIM.particle.weight[x]
+        PRIM.particle.chance[x] = sum / PRIM.particle.total_w
+    }
+}
+
+calcPartChances()
+
+function freePrimordiumParticles(id) {
+	let res = E(0)
+	if (hasTree('prim4') && id == 0) res = player.qu.prim.theorems,player.qu.prim.particles[0] = E(0)
+	if (hasTree('prim5') && id == 2) res = player.qu.prim.theorems,player.qu.prim.particles[2] = E(0)
+	if (hasTree('prim6') && id == 1) res = player.qu.prim.theorems,player.qu.prim.particles[1] = E(0)
+	if (hasTree('prim7') && id == 3) res = player.qu.prim.theorems,player.qu.prim.particles[3] = E(0)
+	if (hasTree('prim8') && id == 4) res = player.qu.prim.theorems,player.qu.prim.particles[4] = E(0)
+	if (hasTree('prim8') && id >= 5) res = player.qu.prim.theorems.div(hasElement(166)?1:hasElement(152)?1.5:hasElement(132)?2:hasPrestige(0,130)?2.5:3).floor(),player.qu.prim.particles[5] = E(0)
+	if (hasPrestige(1,4)) res = res.add(5)
+	if (hasPrestige(1,9)) res = res.add(prestigeEff(1,9,0))
+	return res
+}
+function updatePrimordiumTemp() {
+    tmp.prim.t_base = E(5)
+    if (hasTree('prim1')) tmp.prim.t_base = tmp.prim.t_base.sub(1)
+    tmp.prim.theorems = PRIM.getTheorems()
+    tmp.prim.next_theorem = PRIM.getNextTheorem()
+    tmp.prim.unspent = player.qu.prim.theorems.sub(PRIM.spentTheorems()).max(0)
+    for (let i = 0; i < player.qu.prim.particles.length; i++) {
+        let pp = freePrimordiumParticles(i)
+		pp = pp.add(player.qu.prim.particles[i])
+        if (player.qu.rip.active) pp = pp.mul(i==5?hasElement(95)?0.1:0:1/2)
+        tmp.prim.eff[i] = PRIM.particle.eff[i](pp.softcap(100,0.75,0))
+    }
+}
+
+function updatePrimordiumHTML() {
+    tmp.el.prim_theorem.setTxt(format(player.qu.prim.theorems,0)+"原基定理，其中有"+format(tmp.prim.unspent,0))
+	if (hasTree('prim8')){
+		tmp.el.prim_theorem.setTxt(format(player.qu.prim.theorems,0))
+		tmp.el.primordium_btn.setDisplay(false)
+	}
+    tmp.el.prim_next_theorem.setTxt(format(tmp.prim.next_theorem,1)+"蓝图粒子，目前有"+format(player.qu.bp,1)+"蓝图粒子")
+    for (let i = 0; i < player.qu.prim.particles.length; i++) {
+        tmp.el["prim_part"+i].setTxt(format(player.qu.prim.particles[i],0))
+		if (freePrimordiumParticles(i).gt(0))tmp.el["prim_part"+i].setTxt(format(player.qu.prim.particles[i],0)+" + "+format(freePrimordiumParticles(i),0))
+			if (hasTree('prim8')){
+				let level=player.qu.prim.theorems;
+				if(i>=5)level=level.div(hasElement(166)?1:hasElement(152)?1.5:hasElement(132)?2:hasPrestige(0,130)?2.5:3).floor();
+				tmp.el["prim_part"+i].setTxt("等级 "+format(level,0)+" + "+format(freePrimordiumParticles(i).sub(level),0))
+			}
+        tmp.el["prim_part_eff"+i].setHTML(PRIM.particle.effDesc[i](tmp.prim.eff[i]))
+    }
+}
